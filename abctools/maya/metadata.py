1d733a34abb07ddb7b86b44003743998aa9551b8
Mike Boers <westernx@mikeboers.com>
2015-10-06 17:32:01 -0700

    Serialize everything we get, even if it is wrong

--- abctools/maya/metadata.py
+++ abctools/maya/metadata.py
@@ -3,6 +3,10 @@ import json
 from maya import cmds
 
 
+def _json_default(x):
+    return ('__unjsonable__', x.__class__.__name__, repr(x))
+
+
 def create_metadata_transform(metadata, prefix='__ksmeta__'):
 
     prop_prefix = 'ksmeta'
@@ -13,7 +17,7 @@ def create_metadata_transform(metadata, prefix='__ksmeta__'):
         for key, value in metadata.iteritems():
             attr = '%s_json_%s' % (prop_prefix, key)
             cmds.addAttr(transform, longName=attr, dataType='string')
-            cmds.setAttr('%s.%s' % (transform, attr), json.dumps(value), type='string')
+            cmds.setAttr('%s.%s' % (transform, attr), json.dumps(value, default=_json_default), type='string')
 
     except:
         # Clean up if we didn't manage to create everything.
