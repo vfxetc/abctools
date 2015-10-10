d967307b109ed7470d03eed4a14237b438a233d2
Mark Reid <mindmark@gmail.com>
2015-10-09 18:02:03 -0700

    use embbed tracking sets

--- abctools/commands/abc_nuke_tracker.py
+++ abctools/commands/abc_nuke_tracker.py
@@ -4,8 +4,11 @@ import subprocess
 import tempfile
 import shutil
 import yaml
+import json
+import pprint
 
 from abctools import nuke_tracker
+from abctools import metadata
 
 def convert_camera(path, temp_dir):
     basename = os.path.basename(path)
@@ -16,28 +19,66 @@ def convert_camera(path, temp_dir):
     subprocess.check_call(cmd)
     return dest
 
+def read_tracking_set_meta(path, prefix = "__tracker__"):
+    meta = metadata.load(path)
+
+    data = {'objects':{},
+            'tracks': []}
+
+    for key, value in meta['sets'].items():
+        name = key.split(':')[-1]
+        if not name.lower().startswith(prefix):
+            continue
+
+        # print name.replace(prefix, "")
+        tracking_points = []
+
+        for attr in value['attributes']:
+            object_name = attr.split(':')[-1].split('.')[0]
+
+            if not object_name in data['objects']:
+                data['objects'][object_name] = {'alternative_names': [object_name, object_name + "Shape"]}
+
+            # print object_name
+            for index in value['attributes'][attr].get('indices', []):
+                tracking_points.append([object_name, int(index)])
+            # print indices
+
+        track = {'tracker_name':name.replace(prefix, ""),
+                   'tracking_points':tracking_points}
+
+        data['tracks'].append(track)
+
+    return data
+
 def cli():
-    usage = "usage: %prog [options] SOURCE_ABC_FILE TRACKING_SET.yml  [DEST_NUKE_FILE]"
+    usage = "usage: %prog [options] SOURCE_ABC_FILE [DEST_NUKE_FILE]"
     parser = OptionParser(usage)
     parser.add_option("-c", '--camera', default = None, help = "camera file [.abc/.mb/.ma]")
+    parser.add_option("-t", '--tracking_set', default = None, help = "tracking set file [.yml]")
 
     (options, args) = parser.parse_args()
 
-    if len(args) < 2:
+    if not args:
         parser.error("Not enough arguments")
 
     source = args[0]
-    tracking_sets = yaml.load(file(args[1], 'r'))
 
-    if len(args) > 2:
-        dest = args[2]
+
+    if len(args) > 1:
+        dest = args[1]
     else:
         name, ext = os.path.splitext(source)
         dest = name + ".nk"
 
+    if options.tracking_set:
+        tracking_sets = yaml.load(file(options.tracking_set, 'r'))
+    else:
+        tracking_sets = read_tracking_set_meta(source)
+
+    # pprint.pprint(tracking_sets)
+
     camera_file = source
-
-
 
     temp_dir = tempfile.mkdtemp()
     try:
@@ -47,10 +88,10 @@ def cli():
             if ext.lower() in ('.mb', '.ma'):
                 camera_file = convert_camera(options.camera, temp_dir)
 
-            nuke_tracker.nuke_tracker(abc_file = source,
-                                                            abc_camera_file = camera_file,
-                                                            tracking_sets = tracking_sets,
-                                                            dest_nuke_file = dest)
+        nuke_tracker.nuke_tracker(abc_file = source,
+                                  abc_camera_file = camera_file,
+                                  tracking_sets = tracking_sets,
+                                  dest_nuke_file = dest)
 
     finally:
         shutil.rmtree(temp_dir)
